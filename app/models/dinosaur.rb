d40cf1880f005fb2c49e1b3142e7f9b909aac5dd
Olivier Kermoal <olivier.kermoal@gmail.com>
2022-11-22 11:38:39 +0100

    Set up active storage and cloudinary

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,6 +1,7 @@
 class Dinosaur < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :bookings, dependent: :nullify
+  has_many_attached :photos
 
   validates :address, :name, :species, :price_per_day, presence: true
   validates :description, length: { minimum: 6 }
