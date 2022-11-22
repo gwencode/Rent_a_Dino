cb9cd9c76df67c4e8ac2499a53fd9351d8363ef7
Clement Cordeiro <clement.cordeiro@gmail.com>
2022-11-22 17:56:04 +0100

    display the show and you can create a booking

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,8 +1,11 @@
 class DinosaursController < ApplicationController
+
   def index
   end
 
   def show
+    @dino = Dinosaur.find(params[:id])
+    @booking = Booking.new
   end
 
   def create
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,9 +5,14 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
+  def duration
+    (self.end_date - self.start_date) / 60 / 60 / 24
+  end
+
   private
 
   def end_date_after_start_date
     errors.add(:end_date, "can't be before start date") if end_date < start_date
   end
+
 end
