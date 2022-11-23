7c53d5e3f5c87f216957539bc3a3b13996e470be
Clement Cordeiro <clement.cordeiro@gmail.com>
2022-11-23 12:57:55 +0100

    booking price is dynamix and you can't book if the dates are invalids

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import DisplayPriceController from "./display_price_controller"
+application.register("display-price", DisplayPriceController)
