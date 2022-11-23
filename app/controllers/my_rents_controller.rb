a2d1261d8593f5f8349ccaa6a3b32a99b80a385b
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-23 13:01:22 +0100

    Do My Rents index - waiting for my bookings cards

--- app/controllers/my_rents_controller.rb
+++ app/controllers/my_rents_controller.rb
@@ -1,5 +1,9 @@
 class MyRentsController < ApplicationController
   def index
+    @bookings = Booking.all
+    @dinos = Dinosaur.all
+    # On crée un array vide des bookings du current_user
+    @user_bookings = []
   end
 
   def accept
