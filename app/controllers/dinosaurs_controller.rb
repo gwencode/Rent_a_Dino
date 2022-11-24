da196367f6c9315646e6c36e6c52f428d725557a
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-24 17:31:02 +0100

    add map

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -3,14 +3,6 @@ class DinosaursController < ApplicationController
 
   def index
     @dinos = Dinosaur.all
-    # The `geocoded` scope filters only dinos with coordinates
-    @markers = @dinos.geocoded.map do |dino|
-      {
-        lat: dino.latitude,
-        lng: dino.longitude,
-        info_window: render_to_string(partial: "info_window", locals: {dino: dino})
-      }
-    end
   end
 
   def show
@@ -19,5 +11,17 @@ class DinosaursController < ApplicationController
   end
 
   def create
+  end
+
+  def map
+    @dinos = Dinosaur.all
+    # The `geocoded` scope filters only dinos with coordinates
+    @markers = @dinos.geocoded.map do |dino|
+      {
+        lat: dino.latitude,
+        lng: dino.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {dino: dino})
+      }
+    end
   end
 end
